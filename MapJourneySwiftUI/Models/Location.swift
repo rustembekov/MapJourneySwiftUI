090679eab7d02e90061d257dba1d8ffdc2c41857
Sabyrzhan <rustembekov2003@gmail.com>
2024-06-22 00:01:03 +0500

    Created DataService for Location

---
+++ MapJourneySwiftUI/Models/Location.swift
@@ -0,0 +1,20 @@
+//
+//  Location.swift
+//  MapJourneySwiftUI
+//
+//  Created by Sabr on 21.06.2024.
+//
+
+import Foundation
+import MapKit
+
+
+struct Location {
+    let name: String
+    let cityName: String
+    let coordinates: CLLocationCoordinate2D
+    let description: String
+    let imageNames: [String]
+    let link: String
+    
+}
