d0cf2ca65be9c1280fee77cc40be045568bdad54
Sabyrzhan <rustembekov2003@gmail.com>
2024-06-23 11:31:38 +0500

    Created LocationDetailView

---
+++ MapJourneySwiftUI/Views/LocationDetailView.swift
@@ -0,0 +1,85 @@
+//
+//  LocationDetailView.swift
+//  MapJourneySwiftUI
+//
+//  Created by Sabr on 23.06.2024.
+//
+
+import SwiftUI
+
+struct LocationDetailView: View {
+    let location: Location
+    var body: some View {
+        HStack {
+            VStack(spacing: 16) {
+                imagePreview
+                titlePreview
+            }
+            VStack(alignment: .leading, spacing: 16) {
+                buttonLearnMore
+                buttonNext
+            }
+        }
+    }
+}
+
+struct LocationDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        
+        ZStack {
+            Color.blue
+                .ignoresSafeArea()
+            LocationDetailView(location: LocationsDataService.locations.first!)
+        }
+    }
+}
+
+extension LocationDetailView {
+    private var imagePreview: some View {
+        ZStack {
+            if let imageName = location.imageNames.first {
+                Image(imageName)
+                    .resizable()
+                    .scaledToFit()
+                    .cornerRadius(10)
+                    .frame(width: 100, height: 100)
+            }
+        }
+        .padding(6)
+        .background(Color.white)
+        .cornerRadius(10)
+    }
+    
+    private var titlePreview: some View {
+        VStack(alignment: .leading, spacing: 4) {
+            Text(location.name)
+                .font(.title2)
+                .fontWeight(.black)
+            
+            Text(location.cityName)
+                .font(.headline)
+        }
+    }
+    
+    private var buttonLearnMore: some View {
+        Button {
+            
+        } label: {
+            Text("Learn More")
+                .font(.headline)
+                .frame(width: 125, height: 35)
+        }
+        .buttonStyle(.borderedProminent)
+    }
+    private var buttonNext: some View {
+        Button {
+            
+        } label: {
+            Text("Next")
+                .font(.headline)
+                .frame(width: 125, height: 35)
+        }
+        .buttonStyle(.bordered)
+    }
+    
+}
